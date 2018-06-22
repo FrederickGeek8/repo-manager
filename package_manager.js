5f9551a9051cc22589d441158be8b642908d08c4
Frederick Morlock <FrederickGeek8@gmail.com>
2018-06-22 12:43:07 -0400

    Add a filename cache
    Removes the need to parse Packages everytime

--- package_manager.js
+++ package_manager.js
@@ -1,36 +1,104 @@
+"use strict";
 const dpkg = require("unipkg");
 const fs = require("fs-extra");
 const compressjs = require("compressjs");
 
 // first parse Packages to get the current files
 class PackageManager {
-  static async add(file, disable_bzip=false) {
+  static async add(file, disable_bzip = false) {
+    /*
+      Check that either Packages or Packages.bz2 exist (maybe both) and load
+      the more recent version
+    */
+    const pkgCache =
+      (await fs.stat(".cydia/Packages.json").catch(() => {})) || 0;
+    const pkg = (await fs.stat("Packages").catch(() => {})) || 0;
+    const pkgBz2 = (await fs.stat("Packages.bz2").catch(() => {})) || 0;
+
+    var selected;
+    if (pkgCache.mtime > pkg.mtime && pkgCache.mtime > pkgBz2.mtime) {
+      selected = ".cydia/Packages.json";
+    } else if (pkg.mtime > pkgBz2.mtime && pkg.mtime > pkgCache.mtime) {
+      selected = "Packages";
+    } else {
+      selected = "Packages.bz2";
+    }
+
+    var pkgData = "";
+    if (selected == "Packages.bz2") {
+      await fs
+        .readFile("Packages.bz2")
+        .then(data => {
+          const alg = compressjs.Bzip2;
+          const decompressed = alg.decompressFile(data);
+          pkgData = Buffer.from(decompressed).toString();
+        })
+        .catch(err => {});
+    } else if (selected == "Packages") {
+      await fs
+        .readFile("Packages")
+        .then(data => {
+          // Parse data
+          pkgData = data;
+        })
+        .catch(err => {
+          // do nothing, just continue
+        });
+    } else {
+      await fs
+        .readFile(".cydia/Packages.json")
+        .then(data => {
+          pkgData = JSON.parse(data);
+        })
+        .catch(err => {});
+    }
+
     var files = [file];
-    await fs
-    .readFile("Packages")
-    .then(data => {
-      // Parse data
-      const chunks = data.toString().split("\n\n");
+    if (!Array.isArray(pkgData)) {
+      var chunks = pkgData.toString().split("\n\n");
       for (let i = 0; i < chunks.length; i++) {
         let reg = /(Filename: )+(.+)/g;
-        const match = reg.exec(chunks[i]);
+        var match = reg.exec(chunks[i]);
         // match[2] == filename
         if (match && match[2] && !files.includes(match[2])) {
           files.push(match[2]);
         }
       }
-    }).catch(err => {
-      // do nothing, just continue
-    });
+    }
+
+    // cache results async
+    const prom = fs
+      .readFile(".cydia/Packages.json")
+      .then(data => {
+        var cache = JSON.parse(data); // array of files
+        for (let i = 0; i < files.length; i++) {
+          if (!cache.includes(files[i])) {
+            cache.push(files[i]);
+          }
+        }
+
+        return fs.writeFile(".cydia/Packages.json", JSON.stringify(cache));
+      })
+      .catch(err => {
+        // just write out all files
+        return fs
+          .mkdirp(".cydia")
+          .then(() =>
+            fs.writeFile(".cydia/Packages.json", JSON.stringify(files))
+          );
+      });
 
     await dpkg.scanFiles(files);
 
     if (!disable_bzip) {
       const alg = compressjs.Bzip2;
-      const data = fs.readFileSync("Packages");
-      const compressed = alg.compressFile(data);
-      fs.writeFileSync("Packages.bz2", compressed, 'binary');
+      const pkgData = fs.readFileSync("Packages");
+      const compressed = alg.compressFile(pkgData);
+      fs.writeFileSync("Packages.bz2", compressed, "binary");
+      fs.unlinkSync("Packages");
     }
+
+    return prom;
   }
 }
 
