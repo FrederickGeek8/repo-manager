dc60081f04879da8ce09c4fad4709b8b1c728ccf
Frederick Morlock <FrederickGeek8@gmail.com>
2018-08-16 12:27:28 -0400

    Add list function

--- src/package_manager.js
+++ src/package_manager.js
@@ -1,15 +1,16 @@
 const dpkg = require("unipkg");
 const fs = require("fs-extra");
 const compressjs = require("compressjs");
+const path = require("path");
 
 // first parse Packages to get the current files
 class PackageManager {
-  static async add(file, disable_bzip = false, pathPrefix = "") {
+  static async add(file, disable_bzip = false, pathPrefix = "./") {
     var pkgData = await PackageManager.resolve(pathPrefix);
 
     var files = [file];
     if (Array.isArray(pkgData)) {
-      files = pkgData;
+      files = pkgData.map(x => path.join(pathPrefix, x));
       files.push(file);
     } else {
       var chunks = pkgData.toString().split("\n\n");
@@ -18,21 +19,20 @@ class PackageManager {
         var match = reg.exec(chunks[i]);
         // match[2] == filename
         if (match && match[2] && !files.includes(match[2])) {
-          files.push(match[2]);
+          files.push(path.join(pathPrefix, match[2]));
         }
       }
     }
 
-    return this.save(files, disable_bzip);
+    return this.save(files, disable_bzip, pathPrefix);
   }
 
-  static async remove(file, disable_bzip = false, pathPrefix = "") {
+  static async remove(file, disable_bzip = false, pathPrefix = "./") {
     var pkgData = await PackageManager.resolve(pathPrefix);
 
-    var files = [file];
+    var files = [];
     if (Array.isArray(pkgData)) {
-      files = pkgData;
-      files.push(file);
+      files = pkgData.map(x => path.join(pathPrefix, x));
     } else {
       var chunks = pkgData.toString().split("\n\n");
       for (let i = 0; i < chunks.length; i++) {
@@ -40,7 +40,7 @@ class PackageManager {
         var match = reg.exec(chunks[i]);
         // match[2] == filename
         if (match && match[2] && !files.includes(match[2])) {
-          files.push(match[2]);
+          files.push(path.join(pathPrefix, match[2]));
         }
       }
     }
@@ -52,65 +52,131 @@ class PackageManager {
       throw new Error(`File ${file} not found in Packages index.`);
     }
 
-    return this.save(files, disable_bzip);
+    return this.save(files, disable_bzip, pathPrefix);
   }
 
-  static async save(files, disable_bzip = false, pathPrefix = "") {
-    await dpkg.scanFiles(files);
+  static async list(pathPrefix = "./") {
+    var pkgData;
+    await PackageManager.resolve(pathPrefix, false).then(data => {
+      pkgData = data;
+    });
+
+    // first split then parseControl
+    var chunks = pkgData.toString().split("\n\n");
+    const pkgs = [];
+    for (let i = 0; i < chunks.length - 1; i++) {
+      let dataDict = {
+        Package: "",
+        Version: "",
+        Architecture: "",
+        Maintainer: "",
+        Description: []
+      };
+
+      /*
+        We use this less than ideal regex here to deal with long
+        descriptions that are present in some Debian packages.
+        
+        https://www.debian.org/doc/manuals/maint-guide/dreq.en.html#control
+      */
+
+      var result;
+      var reg = /^(([^\s]*):)?(.+)/gm;
+      while ((result = reg.exec(chunks[i]))) {
+        /*
+          result[1] = Description:
+          result[2] = Description
+          result[3] = virtual dice roller
+        */
+
+        if (result[3]) {
+          if (!result[2] || result[2] == "Description") {
+            dataDict["Description"].push(result[3].trim());
+          } else {
+            dataDict[result[2]] = result[3].trim();
+          }
+        }
+      }
+
+      pkgs.push(dataDict);
+    }
+
+    return pkgs;
+  }
+
+  static async save(files, disable_bzip = false, pathPrefix = "./") {
+    await dpkg.scanFiles(files, pathPrefix);
 
     if (!disable_bzip) {
       const alg = compressjs.Bzip2;
-      const pkgData = fs.readFileSync(`${pathPrefix}Packages`);
+      const pkgData = fs.readFileSync(path.join(pathPrefix, "Packages"));
       const compressed = alg.compressFile(pkgData);
-      fs.writeFileSync(`${pathPrefix}Packages.bz2`, compressed, "binary");
-      fs.unlink(`${pathPrefix}Packages`);
+      fs.writeFileSync(
+        path.join(pathPrefix, "Packages.bz2"),
+        compressed,
+        "binary"
+      );
+      fs.unlink(path.join(pathPrefix, "Packages"));
     } else {
-      fs.unlink(`${pathPrefix}Packages.bz2`).catch(() => {}); // doesn't have to exist
+      fs.unlink(path.join(pathPrefix, "Packages.bz2")).catch(() => {}); // doesn't have to exist
     }
 
     // cache results async
     const prom = fs
-      .mkdirp(".rpo")
+      .mkdirp(path.join(pathPrefix, ".rpo"))
       .then(() =>
-        fs.writeFile(`${pathPrefix}.rpo/Packages.json`, JSON.stringify(files))
+        fs.writeFile(
+          path.join(pathPrefix, ".rpo", "Packages.json"),
+          JSON.stringify(files)
+        )
       );
 
     return prom;
   }
 
-  static async resolve(pathPrefix = "") {
+  static async resolve(pathPrefix = "./", enableCache = true) {
     /*
       Check that either Packages or Packages.bz2 exist (maybe both) and load
       the more recent version
     */
     const pkgCache =
-      (await fs.stat(`${pathPrefix}.rpo/Packages.json`).catch(() => {})) || 0;
-    const pkg = (await fs.stat(`${pathPrefix}Packages`).catch(() => {})) || 0;
+      (await fs
+        .stat(path.join(pathPrefix, ".rpo", "Packages.json"))
+        .then(_ => _.mtime)
+        .catch(() => {})) || 0;
+    const pkg =
+      (await fs
+        .stat(path.join(pathPrefix, "Packages"))
+        .then(_ => _.mtime)
+        .catch(() => {})) || 0;
     const pkgBz2 =
-      (await fs.stat(`${pathPrefix}Packages.bz2`).catch(() => {})) || 0;
+      (await fs
+        .stat(path.join(pathPrefix, "Packages.bz2"))
+        .then(_ => _.mtime)
+        .catch(() => {})) || 0;
 
     var selected;
-    if (pkgCache.mtime > pkg.mtime && pkgCache.mtime > pkgBz2.mtime) {
-      selected = `${pathPrefix}.rpo/Packages.json`;
-    } else if (pkg.mtime > pkgBz2.mtime && pkg.mtime > pkgCache.mtime) {
-      selected = `${pathPrefix}Packages`;
+    if (pkgCache > pkg && pkgCache > pkgBz2 && enableCache) {
+      selected = path.join(pathPrefix, ".rpo", "Packages.json");
+    } else if (pkg > pkgBz2 && (pkg > pkgCache || !enableCache)) {
+      selected = path.join(pathPrefix, "Packages");
     } else {
-      selected = `${pathPrefix}Packages.bz2`;
+      selected = path.join(pathPrefix, "Packages.bz2");
     }
 
     var pkgData = "";
-    if (selected == `${pathPrefix}Packages.bz2`) {
+    if (selected == path.join(pathPrefix, "Packages.bz2")) {
       await fs
-        .readFile(`${pathPrefix}Packages.bz2`)
+        .readFile(path.join(pathPrefix, "Packages.bz2"))
         .then(data => {
           const alg = compressjs.Bzip2;
           const decompressed = alg.decompressFile(data);
           pkgData = Buffer.from(decompressed).toString();
         })
         .catch(err => {});
-    } else if (selected == `${pathPrefix}Packages`) {
+    } else if (selected == path.join(pathPrefix, "Packages")) {
       await fs
-        .readFile(`${pathPrefix}Packages`)
+        .readFile(path.join(pathPrefix, "Packages"))
         .then(data => {
           // Parse data
           pkgData = data;
@@ -120,7 +186,7 @@ class PackageManager {
         });
     } else {
       await fs
-        .readFile(`${pathPrefix}.rpo/Packages.json`)
+        .readFile(path.join(pathPrefix, ".rpo", "Packages.json"))
         .then(data => {
           pkgData = JSON.parse(data);
         })
